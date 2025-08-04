9b87502adc855d093933df8393434ff930823027
bowenzarrow <bootycaga@gmail.com>
2025-08-04 15:37:10 -0400

    basic home page (8/4/25)

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,5 +1,17 @@
+import React from 'react';
+import './pages-css/home.css';
+
 export const Main = () => {
-    return (
-        <div> Home Page</div>
-    )
+  return (
+    <div className="home-container">
+      <video className="background-video" autoPlay loop muted playsInline>
+        <source src="/background.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="overlay">
+        <h1>Welcome to Project Agos</h1>
+        <p>Where we want to fix climate change and stuff</p>
+      </div>
+    </div>
+  );
 }
